75a54d7317dd13c243672c74bb8d0a8629cf0fee
hinaaiga <s1942001nn@s.chibakoudai.jp>
2021-07-16 06:23:03 +0900

    aa

--- htdocs/check.php
+++ htdocs/check.php
@@ -18,14 +18,9 @@
 
         <br>
         <?php
+        
 //データベース接続設定
-$dbServer = '127.0.0.1';
-$dbName = 'ogasawarac';
-$dsn = "mysql:host={$dbServer};
-dbname={$dbName};
-charset=utf8";
-$dbUser = 'test';
-$dbPass = 'pass';
+require 'db.php';
 //データベースへの接続
 $db = new PDO($dsn, $dbUser, $dbPass);
 $sql = 'SELECT * FROM itemtable 
