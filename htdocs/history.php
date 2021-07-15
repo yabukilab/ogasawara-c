c38da6823497e5a4998fcd7e09fabda3b5d0380c
1942049 <72592323+1942049@users.noreply.github.com>
2021-07-15 12:52:54 +0900

    Add files via upload

---
+++ htdocs/history.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <title>履歴確認</title>
+               <link rel="stylesheet" href="jobs.css" >
+    </head>
+    <body>
+        <h1 style="background-color: rgb(130, 131, 130);">
+        <center>履歴確認</center></h1>
+
+        <div class="history2-box">
+        <div class="history1-box";>
+        <span style="color:white">
+        aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+        </span> 
+      </div>
+      </div>
+      <div class="hyou7">
+        <style>
+         section {
+	        overflow: scroll;
+ 	        margin: 10px auto;
+	        padding: 100px;
+	        width: 1000px;
+	        height: 400px;
+	        border: 2px solid #ccc;} 
+          p {	width: 5px;
+              }
+          figure { width: 30px;}
+        </style>
+         <section>
+	      <?php
+
+          ?>
+         </section>
+         
+         <div class="jobs3-box">
+         <button  onclick="location.href='./index.php'" 
+                  class="container1">
+           <a href="#" class="btn-border">  
+             前の画面へ戻る</a>
+         </botton>           
+         </div>
+   </body>
+</html>
---
+++ htdocs/subject.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <title>課題確認</title>
+               <link rel="stylesheet" href="jobs.css" >
+    </head>
+    <body>
+        <h1 style="background-color: rgb(130, 131, 130);">
+        <center>予定確認</center></h1>
+
+      <div class="kadai2-box">
+        <div class="kadai1-box"　
+           style="background-color: rgb(144, 238, 144);">
+        <center>課題</center>
+        </div>
+      </div>
+      <div class="hyou2">
+        <style>
+         section {
+	        overflow: scroll;
+ 	        margin: 10px auto;
+	        padding: 100px;
+	        width: 1000px;
+	        height: 200px;
+	        border: 2px solid #ccc;} 
+          p {	width: 5px;
+              }
+          figure { width: 30px;}
+        </style>
+         <section>
+	      <?php
+
+          ?>
+         </section>
+         <div class="jobs3-box">
+           <div class="container1">
+             <a href="#" class="btn-border">前の画面へ戻る</a>
+           </div>
+        </div>
+
+
+   </body>
+</html>
