44e0773a55d912598d403b6102fb245ee7f5a6eb
hinaaiga <s1942001nn@s.chibakoudai.jp>
2021-07-16 07:21:27 +0900

    a

--- htdocs/check.php
+++ htdocs/check.php
@@ -68,7 +68,7 @@ echo '</table>';
  
         </div> 
         <button  onclick="location.href='./jobs.php'" 
-                   class="btn-push0">仕&emsp;事</button>
+                   class="btn-push0">仕事</button>
             <br><br><br>
         <button  onclick="location.href='./subject.php'" 
                    class="btn-push2">課題</button>
