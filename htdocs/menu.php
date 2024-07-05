1806aef8b124d1dd95823d2245a45d1725fd85c3
Hayuma-Okada <s2242020bb@s.chibakoudai.jp>
2024-07-05 21:37:48 +0900

    a

--- htdocs/menu.php
+++ htdocs/menu.php
@@ -85,11 +85,11 @@ $result_ranking = $conn->query($sql_ranking);
     if (isset($_SESSION['selected_menu_ids']) && !empty($_SESSION['selected_menu_ids'])) {
         foreach ($_SESSION['selected_menu_ids'] as $selected_menu_id) {
             $sql_all_menus = "SELECT m.menu_id, m.menu_name, m.menu_img, 
-                        COALESCE((SELECT COUNT(*) FROM mydb.Rate WHERE menu_id = m.menu_id), 0) AS rating_count, 
-                        COALESCE(ROUND(AVG(r.rate), 1), 0) AS average_rate
-                  FROM mydb.Menu m
-                  LEFT JOIN mydb.Rate r ON m.menu_id = r.menu_id
-                  WHERE m.menu_id = ?";
+                                    COALESCE((SELECT COUNT(*) FROM Rate WHERE menu_id = m.menu_id), 0) AS rating_count, 
+                                    COALESCE(ROUND(AVG(r.rate), 1), 0) AS average_rate
+                              FROM Menu m
+                              LEFT JOIN Rate r ON m.menu_id = r.menu_id
+                              WHERE m.menu_id = ?";
             $stmt = $conn->prepare($sql_all_menus);
             $stmt->bind_param("i", $selected_menu_id);
             $stmt->execute();
