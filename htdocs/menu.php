234884eac74604c4342d1e75d0bf3d5361a92719
Hayuma-Okada <s2242020bb@s.chibakoudai.jp>
2024-07-05 21:10:02 +0900

    aaa

--- htdocs/menu.php
+++ htdocs/menu.php
@@ -74,9 +74,9 @@ if ($result_ranking === false) {
     if (isset($_SESSION['selected_menu_ids']) && !empty($_SESSION['selected_menu_ids'])) {
         foreach ($_SESSION['selected_menu_ids'] as $selected_menu_id) {
             $sql_all_menus = "SELECT m.menu_id, m.menu_name, m.menu_img, 
-                        COALESCE((SELECT COUNT(*) FROM Rate WHERE menu_id = m.menu_id), 0) AS rating_count, 
+                        COALESCE((SELECT COUNT(*) FROM rate WHERE menu_id = m.menu_id), 0) AS rating_count, 
                         COALESCE(ROUND(AVG(r.rate), 1), 0) AS average_rate
-                  FROM Menu m
+                  FROM menu m
                   LEFT JOIN Rate r ON m.menu_id = r.menu_id
                   WHERE m.menu_id = ?";
             $stmt = $conn->prepare($sql_all_menus);
