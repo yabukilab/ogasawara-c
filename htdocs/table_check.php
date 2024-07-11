9896da271d63d4d608bc5cfa7a2e725211b14082
rinamarusuisan <s2242124ur@s.chibakoudai.jp>
2024-07-11 23:28:35 +0900

    a

--- htdocs/table_check.php
+++ htdocs/table_check.php
@@ -12,11 +12,18 @@ if ($conn->connect_error) {
 
 $sql = "SELECT * FROM menu ORDER BY menu_name";
 $stmt = $conn->prepare($sql);
-$stmt -> execute();
+$stmt->execute();
 
-while ($row = $stmt -> fetch(PDO::FETCH_ASSOC )) {
+// 結果の取得
+$result = $stmt->get_result();
+
+while ($row = $result->fetch_assoc()) {
     print_r($row);
     echo("<br/>");
 }
 
+// ステートメントを閉じる
+$stmt->close();
+// 接続を閉じる
+$conn->close();
 ?>
