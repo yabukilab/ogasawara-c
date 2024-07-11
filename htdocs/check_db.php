f13edfd1e31edf4b36b4cad7866c2d69ec8f18ec
rinamarusuisan <s2242124ur@s.chibakoudai.jp>
2024-07-11 20:13:40 +0900

    a

---
+++ htdocs/check_db.php
@@ -0,0 +1,16 @@
+<?php
+require 'db.php';
+
+$sql='SELECT user_id,user_name,user_pass,user_gender FROM users WHERE 1';
+$stmt=$db->prepare($sql);
+$stmt->execute();
+
+$rec=$stmt->fetchall(PDO::FETCH_ASSOC);
+foreach ($rec AS $r){
+    echo $r['user_ide'];
+    echo $r['user_name'];
+    echo $r['user_pass'];
+    echo $r['user_gender'];
+    echo "<br>";
+}
+?>
