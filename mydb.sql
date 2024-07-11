09f4e43a4492c1c47f806e9922b77b6ae6495747
Hayuma-Okada <s2242020bb@s.chibakoudai.jp>
2024-07-12 00:31:12 +0900

    aaa

--- mydb.sql
+++ mydb.sql
@@ -1,164 +1,71 @@
--- MariaDB dump 10.19  Distrib 10.4.32-MariaDB, for Win64 (AMD64)
---
--- Host: localhost    Database: mydb
--- ------------------------------------------------------
--- Server version	10.4.32-MariaDB
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+-- データベースの作成
+CREATE DATABASE IF NOT EXISTS mydb DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 
---
--- Table structure for table `menu`
---
+-- データベースの選択
+USE `mydb`;
 
+-- テーブル menu の作成
 DROP TABLE IF EXISTS `menu`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `menu` (
-  `menu_id` int(11) NOT NULL AUTO_INCREMENT,
-  `menu_name` varchar(50) DEFAULT NULL,
-  `menu_img` mediumblob DEFAULT NULL,
-  `average_rate` decimal(2,1) DEFAULT NULL,
+CREATE TABLE menu (
+  menu_id int(11) NOT NULL AUTO_INCREMENT,
+  menu_name varchar(50) DEFAULT NULL,
+  menu_non varchar(50) DEFAULT NULL,
+  menu_img mediumblob DEFAULT NULL,
+  average_rate DECIMAL(2,1) DEFAULT NULL,
   PRIMARY KEY (`menu_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `menu`
---
-
-LOCK TABLES `menu` WRITE;
-/*!40000 ALTER TABLE `menu` DISABLE KEYS */;
-/*!40000 ALTER TABLE `menu` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Temporary table structure for view `menuwithaveragerate`
---
-
-DROP TABLE IF EXISTS `menuwithaveragerate`;
-/*!50001 DROP VIEW IF EXISTS `menuwithaveragerate`*/;
-SET @saved_cs_client     = @@character_set_client;
-SET character_set_client = utf8;
-/*!50001 CREATE VIEW `menuwithaveragerate` AS SELECT
- 1 AS `menu_id`,
-  1 AS `menu_name`,
-  1 AS `menu_img`,
-  1 AS `average_rate` */;
-SET character_set_client = @saved_cs_client;
-
---
--- Table structure for table `rate`
---
-
+-- テーブル rate の作成
 DROP TABLE IF EXISTS `rate`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `rate` (
-  `rate_id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_id` int(11) DEFAULT NULL,
-  `menu_id` int(11) DEFAULT NULL,
-  `rate` int(11) DEFAULT NULL CHECK (`rate` between 1 and 5),
+CREATE TABLE rate (
+  rate_id int(11) NOT NULL AUTO_INCREMENT,
+  user_id int(11) DEFAULT NULL,
+  menu_id int(11) DEFAULT NULL,
+  rate int(11) DEFAULT NULL CHECK (`rate` between 1 and 5),
   PRIMARY KEY (`rate_id`),
-  UNIQUE KEY `unique_user_menu` (`user_id`,`menu_id`),
-  KEY `menu_id` (`menu_id`),
-  CONSTRAINT `rate_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`),
-  CONSTRAINT `rate_ibfk_2` FOREIGN KEY (`menu_id`) REFERENCES `menu` (`menu_id`)
+  UNIQUE KEY unique_user_menu (`user_id`,`menu_id`),
+  KEY menu_id (`menu_id`),
+  CONSTRAINT fk_rate_menu_id FOREIGN KEY (`menu_id`) REFERENCES menu (`menu_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `rate`
---
+-- テーブル users の作成
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE users (
+  user_id int(11) NOT NULL AUTO_INCREMENT,
+  user_name varchar(50) DEFAULT NULL,
+  user_pass varchar(255) DEFAULT NULL,
+  user_gender enum('male','female','none') DEFAULT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-LOCK TABLES `rate` WRITE;
-/*!40000 ALTER TABLE `rate` DISABLE KEYS */;
-/*!40000 ALTER TABLE `rate` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
-/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
-/*!50003 SET @saved_col_connection = @@collation_connection */ ;
-/*!50003 SET character_set_client  = utf8mb4 */ ;
-/*!50003 SET character_set_results = utf8mb4 */ ;
-/*!50003 SET collation_connection  = utf8mb4_unicode_ci */ ;
-/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
-/*!50003 SET sql_mode              = 'NO_AUTO_VALUE_ON_ZERO' */ ;
-DELIMITER ;;
-/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER update_menu_average_rate AFTER INSERT ON Rate
-FOR EACH ROW
-BEGIN
-    UPDATE Menu m
+-- テーブル menuwithaveragerate_table の作成
+DROP TABLE IF EXISTS `menuwithaveragerate`;
+CREATE TABLE menuwithaveragerate (
+  menu_id int(11) NOT NULL,
+  menu_name varchar(50) DEFAULT NULL,
+  menu_img mediumblob DEFAULT NULL,
+  average_rate DECIMAL(2,1) DEFAULT NULL,
+  PRIMARY KEY (`menu_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- トリガ update_menu_average_rate の作成
+DROP TRIGGER IF EXISTS `update_menu_average_rate`;
+
+DELIMITER $$
+CREATE TRIGGER update_menu_average_rate AFTER INSERT ON rate FOR EACH ROW BEGIN
+    UPDATE menu m
     SET m.average_rate = (
         SELECT COALESCE(ROUND(AVG(r.rate), 1), 0)
-        FROM Rate r
+        FROM rate r
         WHERE r.menu_id = NEW.menu_id
     )
     WHERE m.menu_id = NEW.menu_id;
-END */;;
+END
+$$
 DELIMITER ;
-/*!50003 SET sql_mode              = @saved_sql_mode */ ;
-/*!50003 SET character_set_client  = @saved_cs_client */ ;
-/*!50003 SET character_set_results = @saved_cs_results */ ;
-/*!50003 SET collation_connection  = @saved_col_connection */ ;
 
---
--- Table structure for table `users`
---
-
-DROP TABLE IF EXISTS `users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `users` (
-  `user_id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_name` varchar(50) DEFAULT NULL,
-  `user_pass` varchar(255) DEFAULT NULL,
-  `user_gender` enum('male','female','none') DEFAULT NULL,
-  PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `users`
---
-
-LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Final view structure for view `menuwithaveragerate`
---
-
-/*!50001 DROP VIEW IF EXISTS `menuwithaveragerate`*/;
-/*!50001 SET @saved_cs_client          = @@character_set_client */;
-/*!50001 SET @saved_cs_results         = @@character_set_results */;
-/*!50001 SET @saved_col_connection     = @@collation_connection */;
-/*!50001 SET character_set_client      = utf8mb4 */;
-/*!50001 SET character_set_results     = utf8mb4 */;
-/*!50001 SET collation_connection      = utf8mb4_unicode_ci */;
-/*!50001 CREATE ALGORITHM=UNDEFINED */
-/*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
-/*!50001 VIEW `menuwithaveragerate` AS select `m`.`menu_id` AS `menu_id`,`m`.`menu_name` AS `menu_name`,`m`.`menu_img` AS `menu_img`,coalesce(round(avg(`r`.`rate`),1),0) AS `average_rate` from (`menu` `m` left join `rate` `r` on(`m`.`menu_id` = `r`.`menu_id`)) group by `m`.`menu_id`,`m`.`menu_name`,`m`.`menu_img` */;
-/*!50001 SET character_set_client      = @saved_cs_client */;
-/*!50001 SET character_set_results     = @saved_cs_results */;
-/*!50001 SET collation_connection      = @saved_col_connection */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2024-07-12  0:30:01
+COMMIT;
