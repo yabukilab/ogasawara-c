a303a1d82e4e36d87f3bcd2bc1ae3e4c9e7fe77f
rinamarusuisan <s2242124ur@s.chibakoudai.jp>
2024-07-11 21:27:22 +0900

    a

--- htdocs/jijiji.php
+++ htdocs/jijiji.php
@@ -10,7 +10,7 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$result = $conn->query("SHOW TABLES LIKE 'rate'");
+$result = $conn->query("SHOW TABLES LIKE 'menuwithaveragerate'");
 if ($result->num_rows == 0) {
     die("Table 'rate' doesn't exist.");
 }
