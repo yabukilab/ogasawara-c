23a44ee041e701f7af0e2e37eb83837f775ed376
rinamarusuisan <s2242124ur@s.chibakoudai.jp>
2024-07-11 21:25:46 +0900

    a

--- htdocs/jijiji.php
+++ htdocs/jijiji.php
@@ -1,4 +1,15 @@
 <?php
+session_start();
+require('db.php');
+
+// MySQLデータベースに接続
+$conn = new mysqli($dbServer, $dbUser, $dbPass, $dbName);
+
+// 接続をチェック
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
 $result = $conn->query("SHOW TABLES LIKE 'rate'");
 if ($result->num_rows == 0) {
     die("Table 'rate' doesn't exist.");
