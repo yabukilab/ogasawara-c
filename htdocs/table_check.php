fc2a14af51d6b996b9e519d9bd36efcbef33f441
rinamarusuisan <s2242124ur@s.chibakoudai.jp>
2024-07-12 04:32:25 +0900

    s

--- htdocs/table_check.php
+++ htdocs/table_check.php
@@ -10,17 +10,27 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// カラム名を取得するためのクエリ
-$sql = "SHOW COLUMNS FROM `selected_menus`";
+// テーブルの存在を確認
+$sql = "SHOW TABLES LIKE 'selected_menus'";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
-    // 各カラム名を出力
-    while ($row = $result->fetch_assoc()) {
-        echo $row['Field'] . "<br/>";
+    echo "Table 'selected_menus' exists.<br/>";
+
+    // カラム名を取得するためのクエリ
+    $sql = "SHOW COLUMNS FROM `selected_menus`";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        // 各カラム名を出力
+        while ($row = $result->fetch_assoc()) {
+            echo $row['Field'] . "<br/>";
+        }
+    } else {
+        echo "No columns found.";
     }
 } else {
-    echo "No columns found.";
+    echo "Table 'selected_menus' does not exist.";
 }
 
 // 接続を閉じる
