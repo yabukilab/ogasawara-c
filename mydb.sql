d4f8e945588b76ba6df153509425d4931f1579ce
rinamarusuisan <s2242124ur@s.chibakoudai.jp>
2024-07-12 02:22:18 +0900

    a

--- mydb.sql
+++ mydb.sql
@@ -1,62 +1,108 @@
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+-- MariaDB dump 10.19  Distrib 10.4.32-MariaDB, for Win64 (AMD64)
+--
+-- Host: localhost    Database: mydb
+-- ------------------------------------------------------
+-- Server version	10.4.32-MariaDB
 
--- データベースの作成
-CREATE DATABASE IF NOT EXISTS mydb DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- データベースの選択
-USE `mydb`;
+--
+-- Table structure for table `menu`
+--
 
--- テーブル menu の作成
 DROP TABLE IF EXISTS `menu`;
-CREATE TABLE menu (
-  menu_id int(11) NOT NULL AUTO_INCREMENT,
-  menu_name varchar(50) DEFAULT NULL,
-  menu_non varchar(50) DEFAULT NULL,
-  menu_img mediumblob DEFAULT NULL,
-  average_rate DECIMAL(2,1) DEFAULT NULL,
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `menu` (
+  `menu_id` int(11) NOT NULL AUTO_INCREMENT,
+  `menu_name` varchar(50) DEFAULT NULL,
+  `menu_non` varchar(50) DEFAULT NULL,
+  `menu_img` mediumblob DEFAULT NULL,
+  `average_rate` decimal(2,1) DEFAULT NULL,
   PRIMARY KEY (`menu_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- テーブル rate の作成
-DROP TABLE IF EXISTS `rate`;
-CREATE TABLE rate (
-  rate_id int(11) NOT NULL AUTO_INCREMENT,
-  user_id int(11) DEFAULT NULL,
-  menu_id int(11) DEFAULT NULL,
-  rate int(11) DEFAULT NULL CHECK (`rate` between 1 and 5),
-  PRIMARY KEY (`rate_id`),
-  UNIQUE KEY unique_user_menu (`user_id`,`menu_id`),
-  KEY menu_id (`menu_id`),
-  CONSTRAINT fk_rate_menu_id FOREIGN KEY (`menu_id`) REFERENCES menu (`menu_id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+--
+-- Dumping data for table `menu`
+--
 
--- テーブル users の作成
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE users (
-  user_id int(11) NOT NULL AUTO_INCREMENT,
-  user_name varchar(50) DEFAULT NULL,
-  user_pass varchar(255) DEFAULT NULL,
-  user_gender enum('male','female','none') DEFAULT NULL,
-  PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+LOCK TABLES `menu` WRITE;
+/*!40000 ALTER TABLE `menu` DISABLE KEYS */;
+/*!40000 ALTER TABLE `menu` ENABLE KEYS */;
+UNLOCK TABLES;
 
--- テーブル menuwithaveragerate_table の作成
+--
+-- Table structure for table `menuwithaveragerate`
+--
+
 DROP TABLE IF EXISTS `menuwithaveragerate`;
-CREATE TABLE menuwithaveragerate (
-  menu_id int(11) NOT NULL,
-  menu_name varchar(50) DEFAULT NULL,
-  menu_img mediumblob DEFAULT NULL,
-  average_rate DECIMAL(2,1) DEFAULT NULL,
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `menuwithaveragerate` (
+  `menu_id` int(11) NOT NULL,
+  `menu_name` varchar(50) DEFAULT NULL,
+  `menu_img` mediumblob DEFAULT NULL,
+  `average_rate` decimal(2,1) DEFAULT NULL,
   PRIMARY KEY (`menu_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- トリガ update_menu_average_rate の作成
-DROP TRIGGER IF EXISTS `update_menu_average_rate`;
+--
+-- Dumping data for table `menuwithaveragerate`
+--
 
-DELIMITER $$
-CREATE TRIGGER update_menu_average_rate AFTER INSERT ON rate FOR EACH ROW BEGIN
+LOCK TABLES `menuwithaveragerate` WRITE;
+/*!40000 ALTER TABLE `menuwithaveragerate` DISABLE KEYS */;
+/*!40000 ALTER TABLE `menuwithaveragerate` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `rate`
+--
+
+DROP TABLE IF EXISTS `rate`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `rate` (
+  `rate_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `menu_id` int(11) DEFAULT NULL,
+  `rate` int(11) DEFAULT NULL CHECK (`rate` between 1 and 5),
+  PRIMARY KEY (`rate_id`),
+  UNIQUE KEY `unique_user_menu` (`user_id`,`menu_id`),
+  KEY `menu_id` (`menu_id`),
+  CONSTRAINT `fk_rate_menu_id` FOREIGN KEY (`menu_id`) REFERENCES `menu` (`menu_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `rate`
+--
+
+LOCK TABLES `rate` WRITE;
+/*!40000 ALTER TABLE `rate` DISABLE KEYS */;
+/*!40000 ALTER TABLE `rate` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_unicode_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'NO_ZERO_IN_DATE,NO_ZERO_DATE,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER update_menu_average_rate AFTER INSERT ON rate FOR EACH ROW BEGIN
     UPDATE menu m
     SET m.average_rate = (
         SELECT COALESCE(ROUND(AVG(r.rate), 1), 0)
@@ -64,8 +110,45 @@ CREATE TRIGGER update_menu_average_rate AFTER INSERT ON rate FOR EACH ROW BEGIN
         WHERE r.menu_id = NEW.menu_id
     )
     WHERE m.menu_id = NEW.menu_id;
-END
-$$
+END */;;
 DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 
-COMMIT;
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(50) DEFAULT NULL,
+  `user_pass` varchar(255) DEFAULT NULL,
+  `user_gender` enum('male','female','none') DEFAULT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-07-12  2:20:46
