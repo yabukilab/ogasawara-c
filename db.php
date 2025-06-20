ffbae7efc28c4a76335017221b41a914efd533fa
ShituRiku <s2342065ub@s.chibakougai.jp>
2025-06-20 16:47:29 +0900

    a

--- db.php
+++ db.php
@@ -1,12 +1,13 @@
 <?php
 $host = 'localhost';
-$dbname = 'otoshimono';
-$user = 'root'; 
-$pass = '';     
+$dbname = 'otoshimono'; // あなたのDB名
+$user = 'root';
+$pass = ''; // パスワードがない場合は空文字
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "DB接続成功しました！";
 } catch (PDOException $e) {
     exit('DB接続エラー: ' . $e->getMessage());
 }
