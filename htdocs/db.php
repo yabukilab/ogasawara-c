73858b7374d2fc67d21137a121ca9171a7dccb11
ShituRiku <s2342065ub@s.chibakougai.jp>
2025-06-27 17:22:24 +0900

    a

--- htdocs/db.php
+++ htdocs/db.php
@@ -1,21 +1,14 @@
 <?php
 
-//$host = '127.0.0.1';
-//$dbname = 'mydb';  // あなたのデータベース名
-//$user = 'root';
-//$pass = ''; // XAMPPの初期設定では空
-
 $dbServer = isset($_ENV['MYSQL_SERVER'])    ? $_ENV['MYSQL_SERVER']      : '127.0.0.1';
-$dbUser = isset($_SERVER['MYSQL_USER'])     ? $_SERVER['MYSQL_USER']     : 'root';
-$dbPass = isset($_SERVER['MYSQL_PASSWORD']) ? $_SERVER['MYSQL_PASSWORD'] : '';
-$dbName = isset($_SERVER['MYSQL_DB'])       ? $_SERVER['MYSQL_DB']       : 'mydb';
-
-$dsn = "mysql:host={$dbServer};dbname={$dbName};charset=utf8";
+$dbUser   = isset($_SERVER['MYSQL_USER'])   ? $_SERVER['MYSQL_USER']     : 'root';
+$dbPass   = isset($_SERVER['MYSQL_PASSWORD']) ? $_SERVER['MYSQL_PASSWORD'] : '';
+$dbName   = isset($_SERVER['MYSQL_DB'])     ? $_SERVER['MYSQL_DB']       : 'mydb';
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
+    $pdo = new PDO("mysql:host=$dbServer;dbname=$dbName;charset=utf8", $dbUser, $dbPass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    // echo "DB接続成功しました！"; // ← 本番ではコメントアウト推奨
+    // echo "DB接続成功しました！";
 } catch (PDOException $e) {
     exit('DB接続エラー: ' . $e->getMessage());
 }
