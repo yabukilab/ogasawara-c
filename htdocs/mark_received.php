9c7f428a90a2c883323685da1091b76a26f54232
ShituRiku <s2342065ub@s.chibakougai.jp>
2025-07-03 15:47:27 +0900

    a

--- htdocs/mark_received.php
+++ htdocs/mark_received.php
@@ -1,19 +1,30 @@
 <?php
 require 'db.php';
 
-if (!isset($_POST['id'])) {
+if (!isset($_GET['id'])) {
     header("Location: search.php");
     exit;
 }
 
-$id = $_POST['id'];
-$keyword = $_POST['keyword'] ?? '';
-$location = $_POST['location'] ?? '';
+$id = $_GET['id'];
 
-// is_received を1に更新
-$stmt = $pdo->prepare("UPDATE items SET is_received = 1 WHERE id = ?");
+// IDに対応するデータを取得
+$stmt = $pdo->prepare("SELECT keyword, current_location FROM items WHERE id = ?");
 $stmt->execute([$id]);
+$item = $stmt->fetch(PDO::FETCH_ASSOC);
 
-// 完了画面にリダイレクト
-header("Location: search_complete.php?keyword=" . urlencode($keyword) . "&location=" . urlencode($location));
+// エラー処理
+if (!$item) {
+    exit('対象データが見つかりません');
+}
+
+// is_received を 1 に更新
+$update = $pdo->prepare("UPDATE items SET is_received = 1 WHERE id = ?");
+$update->execute([$id]);
+
+// 完了ページへ遷移（キーワードと現在の場所をURLパラメータで渡す）
+$keyword = urlencode($item['keyword']);
+$location = urlencode($item['current_location']);
+header("Location: search_complete.php?keyword=$keyword&location=$location");
 exit;
+?>
