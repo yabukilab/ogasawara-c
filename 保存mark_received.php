72ad622d9f3ca81291c03d8f0735c6fc3dacad81
aikawatakumu <s2342001ky@s.chibakoudai.jp>
2025-07-10 15:09:48 +0900

    現在の場所の場所に取りに行ってくださいとなあっていたのを修正

--- 保存mark_received.php
+++ 保存mark_received.php
@@ -9,8 +9,7 @@ if (!isset($_GET['id'])) {
 $id = $_GET['id'];
 
 // IDに対応するデータを取得
-// ここで keyword と found_place を取得するように変更します
-$stmt = $pdo->prepare("SELECT keyword, found_place FROM items WHERE id = ?"); // 修正
+$stmt = $pdo->prepare("SELECT keyword, current_location FROM items WHERE id = ?");
 $stmt->execute([$id]);
 $item = $stmt->fetch(PDO::FETCH_ASSOC);
 
@@ -23,10 +22,9 @@ if (!$item) {
 $update = $pdo->prepare("UPDATE items SET is_received = 1 WHERE id = ?");
 $update->execute([$id]);
 
-// 完了ページへ遷移（キーワードと見つけた場所をURLパラメータで渡す）
+// 完了ページへ遷移（キーワードと現在の場所をURLパラメータで渡す）
 $keyword = urlencode($item['keyword']);
-// location パラメータとして found_place を渡します
-$location = urlencode($item['found_place']); // 修正
+$location = urlencode($item['current_location']);
 header("Location: search_complete.php?keyword=$keyword&location=$location");
 exit;
 ?>
